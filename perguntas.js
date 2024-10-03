db57b28d5d6da8f6ae7c8499fea71e3f39aa5fea
Emily <132068134+emily-caputi@users.noreply.github.com>
2024-10-03 08:44:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'Livros',
+    'Quantos livros a Emily tem na casa dela?',
+    'Emily tem 115 livros, ela acha.'
+
+)
+
+criaCartao(
+    'Livros',
+    'Quantos livros a Emily já leu?',
+    '72, informação tirada no skoob.'
+
+)
+
+criaCartao(
+    'Livros',
+    'Qual a Autora favorita da Emily?',
+    'Holly Jackson.'
+
+)
+
+criaCartao(
+    'Livros',
+    'O que a Holly Jackson escreveu?',
+    'Ela escreveu a série Manual de Assassinato Para Boas Garotas.'
+)
+
+criaCartao(
+    'Livros',
+    'Quais os livros favoritos da Emily?',
+    'Coraline, Jogos de herança e Manual de Assassinato Para Boas Garotas.'
+)
+
+criaCartao(
+    'Livros',
+    'Que livro surpreendeu a Emily?',
+    'Noite na taverna, E não sobrou nenhum, a ilha da sete luas.'
+
+)
+
+criaCartao(
+    'Livros',
+    'Como a Emily compra seus livros?',
+    'Olhando a capa e recebendo recomendações no tiktok. É sempre uma surpresa quando ele chegam na sua casa.'
+)
